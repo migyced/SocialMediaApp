6c59ffb5a5f590aa471fdd8513b7c4f9de452558
Miguel Cedillo <82360408+migyced@users.noreply.github.com>
2022-12-11 22:24:55 -0800

    Initial Commit

---
+++ SocialMediaApp/SocialMediaAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SocialMediaAppApp.swift
+//  SocialMediaApp
+//
+//  Created by Miguel Cedillo on 12/11/22.
+//
+
+import SwiftUI
+
+@main
+struct SocialMediaAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
